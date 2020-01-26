84283f425aba5e196e44f9b98d5046e8891475fd
Bruno Rodrigues <bruno@brodrigues.co>
2020-01-26 01:39:06 +0100

    finished cleaning data

--- datasets/process_bus_data.R
+++ datasets/process_bus_data.R
@@ -28,10 +28,70 @@ process_bus_data <- function(data_file){
                        "year of 2nd replacement", "odometer at replacement",
                        "month odometer data begins", "year odometer data begins")
 
+  create_start_date <- function(one_dataset){
+      one_dataset <- pull(one_dataset)
+      month <- one_dataset[10]
+      year <- paste0("19", one_dataset[11])
+
+      month <- ifelse(nchar(month) == 1, paste0("0", month), month)
+
+      ymd(paste0(year, "-", month, "-01"))
+  }
+
+  create_first_replacement <- function(one_dataset){
+      one_dataset <- pull(one_dataset, odometer_reading)
+      month <- one_dataset[4]
+      year <- paste0("19", one_dataset[5])
+
+      month <- ifelse(nchar(month) == 1, paste0("0", month), month)
+
+      ymd(paste0(year, "-", month, "-01"))
+  }
+
+  create_second_replacement <- function(one_dataset){
+      one_dataset <- pull(one_dataset, odometer_reading)
+      month <- one_dataset[7]
+      year <- paste0("19", one_dataset[8])
+
+      month <- ifelse(nchar(month) == 1, paste0("0", month), month)
+
+      ymd(paste0(year, "-", month, "-01"))
+  }
+
+  get_bus_id <- function(one_dataset){
+      one_dataset <- pull(one_dataset, odometer_reading)
+      one_dataset[1]
+  }
+
   named_headers <- map(headers_list, ~set_names(., header_elements))
 
 
+  raw_data <- map(sep_data_sets, ~tibble("odometer_reading" = .))
+  raw_data <- map(raw_data, ~mutate(., "date" = create_start_date(.)))
+  raw_data <- map(raw_data, ~mutate(., "first_replacement_date" = create_first_replacement(.)))
+  raw_data <- map(raw_data, ~mutate(., "second_replacement_date" = create_second_replacement(.)))
+  raw_data <- map(raw_data, ~mutate(., "bus_id" = get_bus_id(.)))
+  raw_data <- map(raw_data, ~slice(., -c(1:11)))
+
+  fill_dates <- function(vector){
+      for(i in 2:length(vector)){
+          vector[i] <- add_with_rollback(vector[i-1], months(1))
+          #vector[i] <- if_else(vector[i] == ymd("1980-07-01"), add_with_rollback(vector[i], months(2)),
+          #                    vector[i])
+      }
+      vector
+  }
+
+  raw_data <- raw_data %>%
+      map(~mutate(., date = fill_dates(date)))
+
+  raw_data <- map(raw_data, ~mutate(., "replacement_1" = if_else(date == first_replacement_date, 1, 0)))
+  raw_data <- map(raw_data, ~mutate(., "replacement_2" = if_else(date == second_replacement_date, 1, 0, 0)))
+  raw_data <- map(raw_data, ~mutate(., replacement = replacement_1 + replacement_2))
+  raw_data <- map(raw_data, ~select(., bus_id, date, odometer_reading, replacement,
+                                    -replacement_1, -replacement_2, -first_replacement_date, -second_replacement_date))
+
+  return(raw_data)
 }
 
-
-headers_list <- map(data_files, process_bus_data)
+processed_data <- map(data_files, process_bus_data)
